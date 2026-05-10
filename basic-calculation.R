af7762ac166196aecd34cb0edf608c72336cd2fe
t-fahim <t.a.fahim@outlook.com>
2026-05-10 19:08:28 +0600

    day 01

---
+++ basic-calculation.R
@@ -0,0 +1,28 @@
+### simple calculation
+
+4+4
+4-4
+4/4
+4^4
+4*4
+
+# square root
+sqrt(16)
+
+# exponential
+exp(4)
+
+# log
+log(10)
+
+# logical operator
+
+4<5
+4<=5
+4>5
+4>=5
+4==5
+4!=5
+
+# modulo
+16%%3
